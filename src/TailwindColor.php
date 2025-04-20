d8cfde66ad69a8a05a37082703a755e698c49589
Keith Andrews <keith@geerservices.com>
2025-04-19 20:44:31 -0400

    Package structure set Test written

---
+++ src/TailwindColor.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace andrewskm\TailwindColors;
+
+enum TailwindColor: string
+{
+    case slate = 'Slate';
+    case gray = 'Gray';
+    case zinc = 'Zinc';
+    case neutral = 'Neutral';
+    case stone = 'Stone';
+    case red = 'Red';
+    case orange = 'Orange';
+    case amber = 'Amber';
+    case yellow = 'Yellow';
+    case lime = 'Lime';
+    case green = 'Green';
+    case emerald = 'Emerald';
+    case teal = 'Teal';
+    case cyan = 'Cyan';
+    case sky = 'Sky';
+    case blue = 'Blue';
+    case indigo = 'Indigo';
+    case violet = 'Violet';
+    case purple = 'Purple';
+    case fuchsia = 'Fuchsia';
+    case pink = 'Pink';
+    case rose = 'Rose';
+
+    /**
+     * Build a tailwind class
+     *
+     * @param  string  $utility
+     * @param  int     $shade
+     *
+     * @return string
+     */
+    public function build(string $utility, int $shade = 500): string
+    {
+        return sprintf('%s-%s-%d', $utility, $this->name, $shade);
+    }
+}
---
+++ tests/TailwindColorTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace andrewskm\TailwindColors\Test;
+
+use andrewskm\TailwindColors\TailwindColor;
+use PHPUnit\Framework\TestCase;
+
+class TailwindColorTest extends TestCase
+{
+    public function test_it_builds_a_utility_class(): void
+    {
+        $cases = TailwindColor::cases();
+        $key   = array_rand(TailwindColor::cases());
+        $test  = $cases[$key];
+
+        $this->assertTrue($test instanceof TailwindColor);
+        $this->assertEquals(sprintf('bg-%s-300', $test->name), $test->build('bg', 300));
+    }
+}
